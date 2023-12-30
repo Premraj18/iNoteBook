34e54237222fc23f9812e8275269670508b8c25d
Premraj18 <premrajprasad18@gmail.com>
2023-12-30 13:29:52 +0530

    develop backend part

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,10 @@ const express = require('express')
 connectToMongo();
 const app = express();
 
-app.get('/',(req,res) => {
-    res.send('Hello World')
-})
+// Available Routes
+
+app.use('/api/auth', require('./routes/Auth'))
+app.use('/api/note', require('./routes/Note'))
 
 app.listen(process.env.PORT,()=>{
     console.log(`listening at http://localhost:${process.env.PORT}`)
